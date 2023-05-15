83b09c0e57221a5106cd44e1ec1bf284a0c582c4
echo <changjiang@edu.com>
2023-05-15 11:50:06 +0800

    Initial commit

---
+++ src/com/leetcode/array/FirstQuestion.java
@@ -0,0 +1,23 @@
+package com.leetcode.array;
+
+/**
+ * @author GongChangjiang
+ * @version 1.0
+ * @Date 2023/5/15
+ * @Description 两数之和
+ */
+public class FirstQuestion {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * @param nums  含有多个数字的数组
+     * @param target 数组中两数之和
+     * @return 数组中两个数加起来等于target的两个数的坐标信息
+     */
+    public int[] twoSum(int[] nums, int target) {
+        
+        return null;
+    }
+}
